ada84a26a89b763d1892e2828991684186182ce2
Oteng Aaron Asiedu <kirchhohhmz@gmail.com>
2025-10-06 18:18:13 +0000

    Create NewsSourceInterface

---
+++ app/Services/Contracts/NewsSourceInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Services\Contracts;
+
+use Illuminate\Support\Collection;
+
+interface NewsSourceInterface
+{
+    public function fetchArticles(string $category = 'general', int $page = 1): Collection;
+    
+    public function searchArticles(string $query, int $page = 1): Collection;
+    
+    public function getSourceName(): string;
+    
+    public function getApiIdentifier(): string;
+}
