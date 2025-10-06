fc7220bdcfc536c06a019d247cdf70b77e9996af
Oteng Aaron Asiedu <kirchhohhmz@gmail.com>
2025-10-06 17:41:46 +0000

    Define authors table schema

--- database/migrations/2025_10_06_173238_create_authors_table.php
+++ database/migrations/2025_10_06_173238_create_authors_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('authors', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->nullable();
             $table->timestamps();
+            
+            $table->index('name');
         });
     }
 
