86cff2210787fa1546fa178c0c8fb7b34bd786c7
Oteng Aaron Asiedu <kirchhohhmz@gmail.com>
2025-10-06 18:16:37 +0000

    Add NewsServiceProvider

---
+++ app/Providers/NewsServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class NewsServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        // Will register news services here
+    }
+
+    public function boot(): void
+    {
+        //
+    }
+}
