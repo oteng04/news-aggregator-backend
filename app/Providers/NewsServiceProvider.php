5b381a598fb8a944fad5114de767a46c642f69be
Oteng Aaron Asiedu <kirchhohhmz@gmail.com>
2025-10-06 18:35:41 +0000

    Register news services in provider

--- app/Providers/NewsServiceProvider.php
+++ app/Providers/NewsServiceProvider.php
@@ -2,13 +2,21 @@
 
 namespace App\Providers;
 
+use App\Services\NewsAPIService;
+use App\Services\GuardianService;
+use App\Services\NYTimesService;
+use App\Services\ArticleAggregatorService;
+use App\Services\Contracts\NewsSourceInterface;
 use Illuminate\Support\ServiceProvider;
 
 class NewsServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
-        // Will register news services here
+        $this->app->singleton(NewsAPIService::class);
+        $this->app->singleton(GuardianService::class);
+        $this->app->singleton(NYTimesService::class);
+        $this->app->singleton(ArticleAggregatorService::class);
     }
 
     public function boot(): void
