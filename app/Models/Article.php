4629b4931f5e6c3bc45ad165a5ebcba7da60f506
Oteng Aaron Asiedu <kirchhohhmz@gmail.com>
2025-10-06 17:51:13 +0000

    Create Article model with relationships

---
+++ app/Models/Article.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'content',
+        'url',
+        'image_url',
+        'published_at',
+        'fetched_at',
+        'source_id',
+        'category_id',
+    ];
+
+    protected $casts = [
+        'published_at' => 'datetime',
+        'fetched_at' => 'datetime',
+    ];
+
+    public function source(): BelongsTo
+    {
+        return $this->belongsTo(Source::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function authors(): BelongsToMany
+    {
+        return $this->belongsToMany(Author::class);
+    }
+}
